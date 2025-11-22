74e6b89f784cceb75bb18a758cc53b1781aafbda
Juann-18 <juan.agudelo.gutierrez@correounivalle.edu.co>
2025-11-21 22:18:32 -0500

    obtener elemento i,j

--- Operaciones.py
+++ Operaciones.py
@@ -1,8 +1,22 @@
-def obtener_elemento(i,j, valores, posicion, cAcumulativo):
+def obtener_elemento_csr(i,j, valores, posicion, cAcumulativo):
 
     for k in range(cAcumulativo[i], cAcumulativo[i+1]):
         if (posicion[k] == j):
-            return valores[j]
+            return valores[k]
     return 0
 
-print(obtener_elemento(1,0,[5, 4, 1, 2],[1, 2, 0, 1],[0, 2, 4, 4]))
+def obtener_elemento_csc(i, j, valores, filas, ccolumnas):
+    for k in range(ccolumnas[j], ccolumnas[j+1]):
+        if filas[k] == i:
+            return valores[k]
+    return 0
+
+
+def obtener_elemento_coo(i, j, valores, filas, columnas):
+
+    for k in range(len(valores)):
+        if filas[k] == i and columnas[k] == j:
+            return valores[k]
+    return 0
+# print(obtener_elemento(1,1,[ 2,4,8,9,1,3,5,6,1,2,4,7,1,1], [1,6,1,2,5,3,0,5,0,1,0,2,5], [0,2,5,6,6,8,10,11,13]
+# ))
--- Proyecto.py
+++ Proyecto.py
@@ -1,6 +1,7 @@
 from CSC import matriz_a_csc
 from CSR import matriz_a_csr
 from COO import matriz_a_coo
+from Operaciones import obtener_elemento_csr, obtener_elemento_coo, obtener_elemento_csc
 
 
 
@@ -13,13 +14,23 @@ def menu():
         print("")
         print("Menu Principal")
         print("1. Obtener la representacion")
+        print("2. Operaciones")
 
         opcion = int(input("Ingrese la opcion: "))
         
         if(opcion == 1 ): 
             submenu(matriz)
+        if(opcion == 2 ):
+            submenu_operaciones(matriz)
 
-
+def agregar_lista(texto):
+    lista = []
+    tamaño = int(input("Ingrese el tamaño del vector: "))
+    print(texto)
+    for i in range(tamaño):
+        n = int(input(f"Ingrese el valor para la posición {i+1}: "))
+        lista.append(n)
+    return lista
 
 
 def agregar_matriz():
@@ -65,6 +76,65 @@ def submenu(matriz):
         print("valores ", val)
         print("filas ", filas)
         print("ccolunas ", ccolumnas)
+
+def submenu_operaciones(matriz):
+    print("")
+    print("1. Obtener elemento")
+    n= int(input("Ingrese la opcion: "))
+    if(n == 1 ):
+        print("")
+        print("1. Utilizar la representacion de la matriz principal(CSR)")
+        print("2. ingresar la representacion")
+        m= int(input("Ingrese la opcion: "))
+        if(m == 1):
+            i = int(input("ingrese el valor de i: "))
+            j = int(input("ingrese el valor de j: "))
+            val, columnas, cfilas = matriz_a_csr(matriz)
+            mostrar_matriz(matriz)
+            print("")
+            print(f'El elemto en la posicion {i},{j} es {obtener_elemento_csr(i,j,val,columnas,cfilas)}')
+
+        if(m == 2):
+            print("1. COO")
+            print("2. CSR")
+            print("3. CSC")
+
+            o = int(input("Ingrese la opcion"))
+            if (o == 1 ):
+                val = agregar_lista("Vector de valores")
+                filas = agregar_lista("Vector de filas")
+                columnas = agregar_lista("Vector de columnas")
+
+                i = int(input("ingrese el valor de i: "))
+                j = int(input("ingrese el valor de j: "))
+                print("")
+                print(f'El elemto en la posicion {i},{j} es {obtener_elemento_coo(i,j,val,filas,columnas)}')
+            elif( o == 2): 
+                
+                val = agregar_lista("Vector de valores")
+                columnas = agregar_lista("Vector de culumnas")
+                cfilas = agregar_lista("Vector de cfilas")
+
+                i = int(input("ingrese el valor de i: "))
+                j = int(input("ingrese el valor de j: "))
+                print("")
+                print(f'El elemto en la posicion {i},{j} es {obtener_elemento_csr(i,j,val,columnas,cfilas)}')
+            elif( o == 3):
+                val = agregar_lista("Vector de valores")
+                filas = agregar_lista("Vector de filas")
+                ccolumnas = agregar_lista("Vector de ccolumna")
+
+                i = int(input("ingrese el valor de i: "))
+                j = int(input("ingrese el valor de j: "))
+                print("")
+                print(f'El elemto en la posicion {i},{j} es {obtener_elemento_csc(i,j,val,filas,ccolumnas)}')
+
+
+
+
+
+
+
 
 
 menu()
